474b36789361362a1f2e2cc55c1f947a67b5cb05
Vikram Grewal <grewav2@student.ubc.ca>
2023-11-20 13:17:46 -0500

    created script to rename data files

---
+++ utility/rename_files.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+folder = str(sys.argv[1])
+# folder = 'hi_hat'
+
+count = 0
+
+folder_path = f'data/_seperated/{folder}'
+files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
+for file_name in files:
+    if count < 10:
+        new_name = f'{folder}_00{count}.wav'
+    elif count >= 10 and count < 100:
+        new_name = f'{folder}_0{count}.wav'
+    else:
+        new_name = f'{folder}_{count}.wav'
+    
+    new_path = os.path.join(folder_path, new_name)
+    os.rename(os.path.join(folder_path, file_name), new_path)
+
+    count += 1
