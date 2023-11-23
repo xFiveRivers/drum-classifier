9a3c262ff20bb9ffc0067265196e120a6ba24444
Vikram Grewal <grewav2@student.ubc.ca>
2023-11-23 11:29:46 -0500

    removed extra column

---
+++ scripts/utility/create_csv.py
@@ -0,0 +1,22 @@
+import csv
+import os
+import re
+import librosa
+
+data_folder = 'data/raw'
+wav_files = os.listdir(data_folder)
+
+concat_result = ' '.join(wav_files)
+class_list = re.findall(r'(\S+)_\d+\.wav', concat_result)
+unique_classes = list(set(class_list))
+
+signal_lengths = []
+
+for f in wav_files:
+    signal, rate = librosa.load('data/raw/'+f, sr=None)
+    signal_lengths.append(signal.shape[0] / rate)
+
+with open('data/samples.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['file', 'length', 'class'])
+    writer.writerows(zip(wav_files, signal_lengths, class_list))
