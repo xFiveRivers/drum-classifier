9836be5d4d4dea190e52467630a6b4c69089b07d
Vikram Grewal <vikramdevgrewal@gmail.com>
2023-12-01 11:46:58 -0500

    added script to downsample

---
+++ scripts/downsample.py
@@ -0,0 +1,12 @@
+import librosa
+import os
+import pandas as pd
+from scipy.io import wavfile
+from tqdm import tqdm
+
+df = pd.read_csv('data/samples.csv', usecols=['file', 'class'])
+
+if len(os.listdir('data/clean')) == 0:
+    for f in tqdm(df.file):
+        signal, rate = librosa.load('data/clean'+f, sr=16000)
+        wavfile.write(filename='data/clean/'+f, rate=rate, data=signal)
