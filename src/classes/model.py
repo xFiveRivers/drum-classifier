1ddfb710ca12dc7c5842d392bc31e060e16c46d2
Vikram Grewal <vikramdevgrewal@gmail.com>
2024-04-22 13:22:36 -0400

    started model development

--- src/classes/model.py
+++ src/classes/model.py
@@ -1,63 +1,102 @@
 import torch
 import torch.nn as nn
+from torchsummary import summary
 
 
 class CNN_Model(nn.Module):
 
-    def __init__(self, n_input: int = 1, n_output: int = 128,
-                 kernal_size: int = 16):
+    def __init__(self, n_input: int = 1, n_classes: int = 3):
         super().__init__()
+
+        # === HYPERPARAMETERS === #
+        N_INPUT = n_input
+        CONV_1_N_OUT = 16
+        CONV_2_N_OUT = 32
+        CONV_3_N_OUT = 64
+        N_OUT = 64
+
+        CONV_KERNAL_SIZE = 3
+        STRIDE = 1
+        PADDING = 2
+        POOL_KERNAL_SIZE = 2
+
+        LINEAR_1_IN = 128 * 17 * 2
+        self.LINEAR_1_OUT = 128
+        self.linear1 = nn.Linear(LINEAR_1_IN, self.LINEAR_1_OUT)
+        LINEAR_2_OUT = 32
+        LINEAR_3_OUT = n_classes
+
+        # === NETWORK LAYERS === #
         self.conv1 = nn.Sequential(
             nn.Conv2d(
-                in_channels = n_input,
-                out_channels = 16,
-                kernel_size = kernal_size,
-                stride = 1,
-                padding = 2
+                in_channels = N_INPUT,
+                out_channels = CONV_1_N_OUT,
+                kernel_size = CONV_KERNAL_SIZE,
+                stride = STRIDE,
+                padding = PADDING
             ),
             nn.ReLU(),
-            nn.MaxPool2d(kernal_size = 2)
+            nn.MaxPool2d(kernel_size = POOL_KERNAL_SIZE)
         )
 
         self.conv2 = nn.Sequential(
             nn.Conv2d(
-                in_channels = 16,
-                out_channels = 32,
-                kernel_size = kernal_size,
-                stride = 1,
-                padding = 2
+                in_channels = CONV_1_N_OUT,
+                out_channels = CONV_2_N_OUT,
+                kernel_size = CONV_KERNAL_SIZE,
+                stride =  STRIDE,
+                padding = PADDING
             ),
             nn.ReLU(),
-            nn.MaxPool2d(kernal_size = 2)
+            nn.MaxPool2d(kernel_size = POOL_KERNAL_SIZE)
         )
 
         self.conv3 = nn.Sequential(
             nn.Conv2d(
-                in_channels = 32,
-                out_channels = 64,
-                kernel_size = kernal_size,
-                stride = 1,
-                padding = 2
+                in_channels = CONV_2_N_OUT,
+                out_channels = CONV_3_N_OUT,
+                kernel_size = CONV_KERNAL_SIZE,
+                stride =  STRIDE,
+                padding = PADDING
             ),
             nn.ReLU(),
-            nn.MaxPool2d(kernal_size = 2)
+            nn.MaxPool2d(kernel_size = POOL_KERNAL_SIZE)
         )
 
         self.conv4 = nn.Sequential(
             nn.Conv2d(
-                in_channels = 64
-                out_channels = n_output,
-                kernel_size = kernal_size,
-                stride = 1,
-                padding = 2
+                in_channels = CONV_3_N_OUT,
+                out_channels = N_OUT,
+                kernel_size = CONV_KERNAL_SIZE,
+                stride =  STRIDE,
+                padding = PADDING
             ),
             nn.ReLU(),
-            nn.MaxPool2d(kernal_size = 2)
+            nn.MaxPool2d(kernel_size = POOL_KERNAL_SIZE)
         )
 
-        self.flatten = nn.Flatten()
-        self.linear = nn.Linear()
+        self.dense = nn.Sequential(
+            nn.Flatten(),
+            self.linear1,
+            nn.Linear(self.LINEAR_1_OUT, LINEAR_2_OUT),
+            nn.Linear(LINEAR_2_OUT, n_classes)
+        )
+
+        self.softmax = nn.Softmax(dim=1)
 
 
     def forward(self, X):
-        pass
+        output = self.conv1(X)
+        output = self.conv2(output)
+        output = self.conv3(output)
+        output = self.conv4(output)
+
+        linear_input = output.size()[1] * output.size()[2] * output.size()[3]
+        self.linear1 = nn.Linear(linear_input, self.LINEAR_1_OUT)
+
+        logits = self.dense(output)
+        pred = self.softmax(logits)
+
+if __name__ == '__main__':
+    cnn = CNN_Model()
+    summary(cnn.cuda(), (1, 256, 6))
