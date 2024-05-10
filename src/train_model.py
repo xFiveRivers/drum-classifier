0a71b402f9df5b4753a14e0bae755a679782621a
Vikram Grewal <vikramdevgrewal@gmail.com>
2024-05-10 15:39:04 -0400

    added docopt capability

--- src/train_model.py
+++ src/train_model.py
@@ -1,18 +1,41 @@
-import torch
+"""
+Model Training Script
+
+Usage:
+    train_model.py --epochs=<epochs> --lr=<lr> --batch=<batch>
+
+Options:
+    -h --help                   Show help screen.
+    --epochs=<epochs>           Number of epochs to train for [default: 10]
+    --lr=<lr>                   Learning rate for optimizer [default: 0.01]
+    --batch=<batch>             Samples per training batch [default: 32]
+"""
+
+
 import torch.nn as nn
 import torch.optim as optim
 from classes import DrumTrackerDataset, ModelTrainer
+from docopt import docopt
 from models import Model_00
 
 
-def main():
+# Parse args from CLI
+args = docopt(__doc__)
+
+
+def main(epochs, lr, batch):
+    # Instatiate classes
     dataset = DrumTrackerDataset()
     model = Model_00()
+
+    # Get loss and optimizer functions
     loss_fn = nn.CrossEntropyLoss()
-    optim_fn = optim.Adam(params=model.parameters(), lr=0.001)
+    optim_fn = optim.Adam(params=model.parameters(), lr=lr)
+
+    # Train model
     trainer = ModelTrainer(model, loss_fn, optim_fn, dataset)
-    trainer.train_model(EPOCHS=10, BATCH_SIZE=64)
+    trainer.train_model(EPOCHS=epochs, BATCH_SIZE=batch)
 
 
 if __name__ == '__main__':
-    main()
+    main(int(args['--epochs']), float(args['--lr']), int(args['--batch']))
