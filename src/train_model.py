8adcdb1e21814021b14375f8ed372196c9b370b0
Vikram Grewal <vikramdevgrewal@gmail.com>
2024-05-10 14:34:29 -0400

    adjusted batch size and learning rate

--- src/train_model.py
+++ src/train_model.py
@@ -1,11 +1,16 @@
 import torch
+import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
-from src.classes import DrumTrackerDataset, CNN_Model
+from classes import DrumTrackerDataset, CNN_Model, ModelTrainer
 
 
 def main():
-    pass
+    dataset = DrumTrackerDataset()
+    cnn_model = CNN_Model()
+    loss_fn = nn.CrossEntropyLoss()
+    optim_fn = optim.Adam(params=cnn_model.parameters(), lr=0.001)
+    trainer = ModelTrainer(cnn_model, loss_fn, optim_fn, dataset)
+    trainer.train_model(EPOCHS=10, BATCH_SIZE=64)
 
 
 if __name__ == '__main__':
